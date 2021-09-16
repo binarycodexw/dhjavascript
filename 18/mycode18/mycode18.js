4a5ce49e2db0cd1fdf9580aa054c3732eb37db6d
binarycodexw <86565924+binarycodexw@users.noreply.github.com>
2021-09-17 00:02:07 +0200

    Add files via upload

---
+++ 18/mycode18/mycode18.js
@@ -0,0 +1,28 @@
+function memoize(fn) {
+  let cache = {};
+
+  return function (number) { 
+    {if (number in cache) {
+      console.log('Fetching from cache for' + number);
+      return cache[number];
+    }
+
+    console.log('Calculating result for' + number);
+    const calculation = fn(number);
+    cache[number] = calculation;
+    return calculation;
+  }}
+}
+
+function factorial(x) {
+  if (x === 0) {
+    return 1;
+  }
+
+  return x * factorial(x - 1);
+}
+
+factorial = memoize(factorial);
+console.log(factorial(10));
+console.log(factorial(6));
+console.log(factorial(5));
